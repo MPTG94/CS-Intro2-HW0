08adb39021dc906261a28ac84f35a9e8fe6f369c
Mor Paz <1912.mor@gmail.com>
2020-03-18 17:37:31 +0200

    Initial commit of hw0, working version

---
+++ hw0q1.c
@@ -0,0 +1,19 @@
+//
+// Created by Mor on 18/03/2020.
+//
+
+#include <stdio.h>
+
+int main() {
+    // Counting from 3
+    for (int i = 3; i > 0; i--) {
+        printf("%d\n", i);
+    }
+    printf("Are you ready to rumble? Ohh yeah!\n");
+
+    /*
+     Well,
+     Are you ready?
+     */
+    return 0;
+}
